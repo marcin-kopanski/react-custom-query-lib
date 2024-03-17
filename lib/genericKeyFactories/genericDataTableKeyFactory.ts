8ee144cdaa33b5b001970d9b6139d70f21d3f8e4
Marcin Kopański <marcin.kopanski@gmail.com>
2024-03-17 22:40:23 +0100

    generic key factory classes wip

--- lib/genericKeyFactories/genericDataTableKeyFactory.ts
+++ lib/genericKeyFactories/genericDataTableKeyFactory.ts
@@ -1,9 +1,23 @@
 import { DataTableReducerState } from "lib/filterReducers/DataTableReducer/types";
-import { genericKeyFactory } from "lib/genericKeyFactories/genericKeyFactory";
+import { GenericKey, GenericKeyFactory } from "lib/genericKeyFactories/genericKeyFactory";
 
-export const genericDataTableKeyFactory = (domain: string) => ({
-  ...genericKeyFactory(domain),
-  pageData: (filter: DataTableReducerState) =>
-    [{ ...genericKeyFactory(domain).list(), name: "page-data", filter }] as const,
-  dataCount: () => [{ ...genericKeyFactory(domain).list(), name: "data-count" }] as const,
-});
+export interface GenericDataTableKey extends GenericKey {
+  filter: DataTableReducerState;
+}
+
+export class GenericDataTableKeyFactory extends GenericKeyFactory {
+  pageData(filter: DataTableReducerState): GenericDataTableKey {
+    return { ...this.list("page-data"), filter } as const;
+  }
+
+  dataCount(): GenericKey {
+    return { ...this.list("data-count") } as const;
+  }
+}
+
+// export const genericDataTableKeyFactory = (domain: string) => ({
+//   ...genericKeyFactory(domain),
+//   pageData: (filter: DataTableReducerState) =>
+//     [{ ...genericKeyFactory(domain).list(), name: "page-data", filter }] as const,
+//   dataCount: () => [{ ...genericKeyFactory(domain).list(), name: "data-count" }] as const,
+// });
--- lib/genericKeyFactories/genericKeyFactory.ts
+++ lib/genericKeyFactories/genericKeyFactory.ts
@@ -1,5 +1,27 @@
-export const genericKeyFactory = (domain: string) => ({
-  base: { domain } as const,
-  list: () => ({ ...genericKeyFactory(domain).base, type: "list" }) as const,
-  element: () => ({ ...genericKeyFactory(domain).base, type: "element" }) as const,
-});
+export interface GenericKey {
+  baseName: string;
+  type: string;
+  name: string;
+}
+
+export class GenericKeyFactory {
+  baseName: string;
+
+  constructor(baseName: string) {
+    this.baseName = baseName;
+  }
+
+  element(name: string): GenericKey {
+    return { baseName: this.baseName, type: "element", name } as const;
+  }
+
+  list(name: string): GenericKey {
+    return { baseName: this.baseName, type: "list", name } as const;
+  }
+}
+
+// export const genericKeyFactory = (domain: string) => ({
+//   base: { domain } as const,
+//   list: () => ({ ...genericKeyFactory(domain).base, type: "list" }) as const,
+//   element: () => ({ ...genericKeyFactory(domain).base, type: "element" }) as const,
+// });
